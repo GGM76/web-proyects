4accd3dc8b7fc5436a356f11e1eb945e0f15111c
GGMeraz <ggmeraz76@gmail.com>
2023-10-11 16:20:04 -0600

    rutas con mvc

---
+++ 02-knex-api/controllers/homeController.js
@@ -0,0 +1,7 @@
+const createHome = (req, res) => {
+  res.send({ message: 'Home created(FAKE)' })
+}
+
+module.exports = {
+  createHome
+}
---
+++ 02-knex-api/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const homeRoutes = require('./routes/homeRoutes')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/api/v1', homeRoutes)
+// routes
+
+app.listen(3000, () => console.log('Server on : 3000'))
