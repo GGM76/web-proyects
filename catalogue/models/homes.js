4fc7c5a43ab3d31d46aa4c4aca7070220351571d
GGMeraz <ggmeraz76@gmail.com>
2023-10-13 16:30:53 -0600

    inicio de catalogo

---
+++ catalogue/models/homes.js
@@ -0,0 +1,63 @@
+// El modelo trae los datos de la base de datos.
+// NO se encarga de validar datos, ni de resolver promesas.
+
+// Paso #1 Necesito traer la configuración del entorno de knex y los detalles de la conexión a la base de datos.
+const knex = require('../config')
+
+// Paso #2 Crear una función que me permita insertar un registro en la tabla homes.
+
+const create = (body) => {
+  return knex
+    .insert(body) // ¿Qué datos voy a insertar?
+    .into('products') // ¿En qué tabla? - Homes
+    .returning(
+      ['house_id', 'title', 'description', 'guests', 'address', 'rental_price', 'fk_user', 'active', 'created_at']) // ¿Qué quiero que me regrese?
+}
+
+const findAll = () => {
+  return knex
+    .select('*')
+    .from('homes')
+    .where('active', true) // Traemos solo los campos a los que no hayamos hecho soft delete
+}
+
+const findOne = (houseId) => {
+  return knex
+    .select(['house_id', 'title', 'description', 'guests', 'address', 'rental_price', 'fk_user', 'active', 'created_at'])
+    .from('homes')
+    .where({ house_id: houseId })
+    .where('active', true)
+}
+
+const update = (houseId, body) => {
+  return knex
+    .update(body)
+    .from('homes')
+    .where({ house_id: houseId })
+    .returning(['house_id', 'title', 'description', 'guests', 'address', 'rental_price', 'fk_user', 'active', 'created_at'])
+}
+
+// Voy a borrar un registro de manera REAL de la base de datos del
+const destroy = (houseId) => {
+  return knex
+    .del() // delete
+    .from('homes')
+    .where({ house_id: houseId })
+}
+
+// Borrado lógico, solo cambio active de true a false
+const softDelete = (houseId) => {
+  return knex
+    .update({ active: false })
+    .from('homes')
+    .where({ house_id: houseId })
+}
+
+module.exports = {
+  create,
+  findAll,
+  findOne,
+  update,
+  destroy,
+  softDelete
+}
---
+++ scarp/index.js
@@ -0,0 +1,45 @@
+import puppeteer from "puppeteer";
+
+const getQuotes = async () => {
+  // Start a Puppeteer session with:
+  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  await page.locator('input').fill('value')
+  // Open a new page
+  const page = await browser.newPage();
+
+  // On this new page:
+  // - open the "http://quotes.toscrape.com/" website
+  // - wait until the dom content is loaded (HTML is ready)
+  await page.goto("https://www.mercadolibre.com.mx/",{
+  //await page.goto("http://quotes.toscrape.com/", {
+    waitUntil: "domcontentloaded",
+  });
+
+// Get page data
+const quotes = await page.evaluate(() => {
+    // Fetch the first element with class "quote"
+    const quote = document.querySelector(".nav-search-input");
+
+    // Fetch the sub-elements from the previously fetched quote element
+    // Get the displayed text and return it (`.innerText`)
+    const text = quote.querySelector(".text").innerText;
+    const author = quote.querySelector(".author").innerText;
+
+    return { text, author };
+  });
+
+  // Display the quotes
+  console.log(quotes);
+
+  // Close the browser
+  await browser.close();
+};
+
+// Start the scraping
+getQuotes();
