0fad0dbe471fe791a2d9e7838a27dfb3c3774844
GGM76 <ggmeraz76@gmail.com>
2023-11-27 20:35:49 -0600

    Middlewares

--- tareas/backend/controllers/tareasController.js
+++ tareas/backend/controllers/tareasController.js
@@ -1,17 +1,21 @@
 
-const getTareas = (req,res) => {
+const getTareas = async (req,res) => {
     res.status(200).json({message:"Tareas"})    
 }
 
-const createTarea = (req,res) => {
+const createTarea = async (req,res) => {
+    if(!req.body.texto){
+        res.status(400)
+        throw new Error('No escrbiste una descripcion')
+    }
     res.status(201).json({message:"Tarea creada"})        
 }
 
-const updateTarea = (req,res) => {
+const updateTarea = async (req,res) => {
     res.status(200).json({message:`Tarea modificada ${req.params.id}`})
 }
 
-const deleteTarea = (req,res) => {
+const deleteTarea = async (req,res) => {
     res.status(200).json({message:`Tarea eliminada ${req.params.id}`})
 }
 
--- tareas/backend/server.js
+++ tareas/backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 
 const port = process.env.PORT ||  5000
 
 const app = express()
 
-app.use('/api/tareas',require('./routes/tareasRoutes'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
+app.use('/api/tareas',require('./routes/tareasRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Servidor correindo en ${port}`))
 
