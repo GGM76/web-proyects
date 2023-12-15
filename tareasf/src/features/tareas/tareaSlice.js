0119088f35010267b8597fa6643362e24965b2dd
GGMeraz <ggmeraz76@gmail.com>
2023-12-15 12:58:50 -0600

    terminado

--- tareasf/src/features/tareas/tareaSlice.js
+++ tareasf/src/features/tareas/tareaSlice.js
@@ -20,6 +20,27 @@ export const crearTarea = createAsyncThunk('tareas/crear', async (tareaData, thu
     }
 })
 
+//Obtener las tareas del usuario 
+export const getTareas = createAsyncThunk('taeras/getTareas', async (_,thunkAPI)=> {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await tareaService.getTareas(token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWihtValue(message)
+    }
+})
+
+//borrar tarea 
+export const deleteTarea = createAsyncThunk('tareas/delete', async (id, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await tareaService.deleteTarea(id, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWihtValue(message)
+    }
+})
 export const tareaSlice = createSlice({
     name: 'tarea',
     initialState,
@@ -36,7 +57,20 @@ export const tareaSlice = createSlice({
                 state.isSucces= true
                 state.tareas.push(action.payload)
             })
-            .addCase(crearTarea.pending, (state) =>{
+            .addCase(crearTarea.rejected, (state,action) =>{
+                state.isLoading= false
+                state.isError= true
+                state.message = action.payload
+            })
+            .addCase(getTareas.pending, (state) =>{
+                state.isLoading= true
+            })
+            .addCase(getTareas.fulfilled, (state) =>{
+                state.isLoading= false
+                state.isSucces= true
+                state.mistareas= action.payload
+            })
+            .addCase(getTareas.rejected, (state,action) =>{
                 state.isLoading= false
                 state.isError= true
                 state.message = action.payload
--- tareasf/src/pages/Dashboard.jsx
+++ tareasf/src/pages/Dashboard.jsx
@@ -1,18 +1,37 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import TareaForm from '../components/TareaForm'
+import Spinner from '../components/Spinner'
+import { getTareas, reset } from "../features/tareas/tareaSlice"
+import TareaItem from '../components/TareaItem'
 
 const Dashboard = () => {
 //proteger el dashboard
   const navigate = useNavigate()
+  const dispatch = useDispatch(state)
+  
   const user = useSelector((state) => state.auth)
+  const {mistareas, isLoading, isError, message }= useSelector((state)=>state.tarea)
+
 
   useEffect(() =>{
+    if(isError){
+      console.log(message)
+    }
     if(!user){
       navigate('/login')
-    } 
-  },[user,navigate])
+    } else{
+      dispatch(getTareas)
+    }
+    return ()=>{
+      dispatch(reset())
+    }
+  },[user,navigate, isError, message, dispatch])
+
+  if(isLoading){
+    return <Spinner />
+  }
   // Si quitamos la proteccion podemos ver el dashboard
   return (
     <>
@@ -21,6 +40,16 @@ const Dashboard = () => {
         <p>Dashboard de tareas</p>
       </section>
         <TareaForm />
+
+        <section className="content">
+          {mistareas.length>0 ? 
+          ( <div className="tareas">
+              {mistareas.map((tarea) =>(
+                <TareaItem key={tarea._id} tarea={tarea} />
+              ))}
+          </div> ) : 
+          (<h3> No tiene tareas </h3>)}
+        </section>
     </>
   )
 }
